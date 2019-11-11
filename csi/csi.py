c72e33fd5710c048170b61f2f574fadaefb18e36
Jimmy CM Yang (楊昌謀) <jimmycm.yang@moxa.com>
2019-11-11 15:30:20 +0800

    plot , timer and ping

--- csi/csi.py
+++ csi/csi.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import struct
 import math
@@ -5,14 +6,13 @@ from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
 from scipy.stats import norm
-import threading
+import queue  
+from threading import Timer,Thread,Event
 
 # for debugging
 import ptvsd
 
 SERVER_IP = "192.168.127.225"
-#STA1_IP = "192.168.127.235"
-#STA1_LEN = "100"
 CSI_PORT = 3490
 
 # typedef struct
@@ -46,14 +46,25 @@ CSI_PORT = 3490
 #     int imag;
 # }COMPLEX;
 
-imagePath = "enter the path to your image here"
 
-class ImgWidget1(QtGui.QLabel):
+class perpetualTimer():
 
-    def __init__(self, parent=None):
-        super(ImgWidget1, self).__init__(parent)
-        pic = QtGui.QPixmap(imagePath)
-        self.setPixmap(pic)
+   def __init__(self,t,hFunction):
+      self.t=t
+      self.hFunction = hFunction
+      self.thread = Timer(self.t,self.handle_function)
+
+   def handle_function(self):
+      self.hFunction()
+      self.thread = Timer(self.t,self.handle_function)
+      self.thread.start()
+
+   def start(self):
+      self.thread.start()
+
+   def cancel(self):
+      self.thread.cancel()
+
 
 class IwCSI(QtCore.QThread):
     update = QtCore.pyqtSignal(tuple)
@@ -105,43 +116,11 @@ class IwCSI(QtCore.QThread):
         except Exception as e:
             print(e)
 
-    def cal_esnr(self, csi):
+    def cal_amp(self, csi):
         try:
-            ber_bpsk = norm.sf(np.sqrt(np.absolute(csi)*2))
-            avg_ber_bpsk = np.mean(ber_bpsk)
-            esnr_bpsk = (norm.isf(avg_ber_bpsk)**2)/2
-
-            ber_qpsk = norm.sf(np.sqrt(np.absolute(csi)))
-            avg_ber_qpsk = np.mean(ber_qpsk)
-            esnr_qpsk = norm.isf(avg_ber_qpsk)**2
-
-            ber_16qam = 0.75 * norm.sf(np.sqrt(np.absolute(csi)/5))
-            avg_ber_16qam = np.mean(ber_16qam)
-            esnr_16qam = (norm.isf(avg_ber_16qam/0.75)**2)*5
-
-            ber_64qam = (7/12) * norm.sf(np.sqrt(np.absolute(csi)/21))
-            avg_ber_64qam = np.mean(ber_64qam)
-            esnr_64qam = (norm.isf(avg_ber_64qam*12/7)**2)*21
-
-            # print('esnr_bpsk={}'.format(esnr_bpsk))
-            # print('esnr_qpsk={}'.format(esnr_qpsk))
-            # print('esnr_16qam={}'.format(esnr_16qam))
-            # print('esnr_64qam={}\n'.format(esnr_64qam))
-            return (esnr_bpsk, esnr_qpsk, esnr_16qam, esnr_64qam)
-        except Exception as e:
-            print(e)
-
-    # Power Delay Profile (Channel Impulse Response)
-    def cal_pdp(self, csi):
-        try:
-            num_tones = len(csi)
-            delta_f = 312.5 * 1000
-            B = delta_f * (num_tones-1)
-            delta_t = 1/B
-            x_cir = [i*delta_t for i in range(num_tones)]
-            y_cir = np.fft.ifft(csi)
-            y_cir = 10*np.log10(np.absolute(y_cir))
-            return (x_cir, y_cir)
+            x_idx = [i for i in range(len(csi))]
+            y_amp = 10*np.log10(np.absolute(csi))
+            return (x_idx, y_amp)
         except Exception as e:
             print(e)
 
@@ -187,39 +166,28 @@ class IwCSI(QtCore.QThread):
         self.sock.sendto(STOP_CSI, (self.server_ip, CSI_PORT))
 
     def run(self):
-        ptvsd.debug_this_thread()
         self.trigger_udp()
         self.recv_data()
 
 
-class CSIPlot(pg.PlotWidget):
+class CSIPlot(pg.GraphicsWindow):
     def __init__(self, parent = None):
         super(CSIPlot, self).__init__(parent)
-        self.showGrid(y=1, alpha=1)
-        self.curve = self.plot(pen='y')
-        self.axis_text = pg.TextItem('')
-        self.addItem(self.axis_text)
+        #self.showGrid(y=1, alpha=1)
+        self.p1 = self.addPlot()
+        self.curve = self.p1.plot()
+        self.curve2 = self.p1.plot()
+        self.curve3 = self.p1.plot()
+        #self.addItem(self.axis_text)
         self.vLine = pg.InfiniteLine(angle=90, movable=False)
         self.hLine = pg.InfiniteLine(angle=0, movable=False)
-        self.addItem(self.vLine, ignoreBounds=True)
-        self.addItem(self.hLine, ignoreBounds=True)
-        self.vb = self.getViewBox()
-        self.proxy = pg.SignalProxy(self.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
         self.x_format = '{:.1f}'
         self.y_format = '{:.1f}'
-    
-    def mouseMoved(self, evt):
-        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
-        if self.sceneBoundingRect().contains(pos):
-            mousePoint = self.vb.mapSceneToView(pos)
-            self.vLine.setPos(mousePoint.x())
-            self.hLine.setPos(mousePoint.y())
-            self.axis_text.setText('[x, y] = [' + self.x_format.format(mousePoint.x()) + ', ' + self.y_format.format(mousePoint.y()) + ']')
-            p = self.vb.viewRange()
-            self.axis_text.setPos(0.8*p[0][1], p[1][1])
 
 
 class CSIWidget(QtGui.QWidget):
+    draw_amp_signal = QtCore.pyqtSignal()
+
     def __init__(self, parent = None, title = '', server_ip = SERVER_IP):
         super(CSIWidget, self).__init__(parent)
         self.started = False
@@ -372,14 +340,11 @@ class CSIWidget(QtGui.QWidget):
         self.table_init()
 
         self.csi_amp = CSIPlot()
-        self.csi_amp.setTitle(title + ' (Amplitude)')
-        self.csi_amp.setXRange(0, 113)
-        self.csi_amp.setYRange(0, 50)
-        self.csi_amp.setLabel('left', 'Amplitude', 'dBm')
-        self.csi_amp.setLabel('bottom', 'Subcarrier Index')
-        self.esnr_text = pg.TextItem('')
-        self.csi_amp.addItem(self.esnr_text)
-        self.esnr_text.setPos(0, 50)
+        #self.csi_amp.setWindowTitle(title + ' (Amplitude)')
+        #self.csi_amp.setXRange(0, 113)
+        #self.csi_amp.setYRange(0, 50)
+        #self.csi_amp.setLabel('left', 'Amplitude', 'dB')
+        #self.csi_amp.setLabel('bottom', 'Subcarrier Index')
 
         self.v2 = QtGui.QVBoxLayout()
         self.v2.addWidget(self.csi_amp)
@@ -393,7 +358,15 @@ class CSIWidget(QtGui.QWidget):
         self.pause_btn.clicked.connect(self.pause_func)
         self.start_btn.clicked.connect(self.start_func)
         self.stop_btn.clicked.connect(self.stop_func)
-    
+
+        self.timer = perpetualTimer(0.5,self.timer_cb)
+        self.q0 = queue.Queue(maxsize = -1)
+        self.q1 = queue.Queue(maxsize = -1)
+        self.q2 = queue.Queue(maxsize = -1)
+        self.draw_amp_signal.connect(self.draw_amp_cb)
+
+
+
     def table_init(self):
         table = QtGui.QTableWidget()
 
@@ -443,6 +416,7 @@ class CSIWidget(QtGui.QWidget):
         table.horizontalHeader().setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
         self.table = table
 
+
     def pause_func(self):
         self.pause = not self.pause
 
@@ -455,6 +429,11 @@ class CSIWidget(QtGui.QWidget):
         self._csi.update.connect(self.update)
         self._csi.start()
         self.started = True
+        self.timer.start()
+        os.system('killall -9 fping')
+        os.system('killall -9 fping')
+        os.system('fping 192.168.127.235 -l -p 100 -b 1 > /dev/null &')
+        os.system('fping 192.168.127.236 -l -p 100 -b 2 > /dev/null &')
         return
 
     def stop_func(self):
@@ -462,90 +441,95 @@ class CSIWidget(QtGui.QWidget):
             return
         self._csi.stop()
         self.started = False
-        return
+        self.timer.cancel()
+        os.system('killall -9 fping')
 
-    def train_start_func(self):
-        self.training_btn.setEnabled(False)
-        self.training_btn.setText("Training\nProcessing")
-        self.testing_btn.setEnabled(False)
-        self.tb_ap.setEnabled(False)
-        self.tb_sta1ip.setEnabled(False)
-        self.tb_sta1len.setEnabled(False)
-        self.tb_sta2ip.setEnabled(False)
-        self.tb_sta2len.setEnabled(False)
-        self.tb_sta3ip.setEnabled(False)
-        self.tb_sta3len.setEnabled(False)
-        timer = threading.Timer(3, self.train_end_timer)
-        timer.start()
-        return
+    def draw_amp_cb(self):
 
-    def train_end_timer(self):
-        self.training_btn.setEnabled(True)
-        self.training_btn.setText("Training\nComplete")
-        self.testing_btn.setEnabled(True)
-        self.tb_sta1ip.setEnabled(True)
-        self.tb_sta1len.setEnabled(True)
-        self.tb_sta2ip.setEnabled(True)
-        self.tb_sta2len.setEnabled(True)
-        self.tb_sta3ip.setEnabled(True)
-        self.tb_sta3len.setEnabled(True)
-        return
+        self.csi_amp.curve.clear()
+        self.csi_amp.curve2.clear()
+        self.csi_amp.curve3.clear()
 
-    def test_func(self):
-        if (self.testing_btn.isChecked()):
-            self.testing_btn.setText("Test\nStop")
-            self.training_btn.setEnabled(False)
-            self.tb_sta1ip.setEnabled(False)
-            self.tb_sta1len.setEnabled(False)
-            self.tb_sta2ip.setEnabled(False)
-            self.tb_sta2len.setEnabled(False)
-            self.tb_sta3ip.setEnabled(False)
-            self.tb_sta3len.setEnabled(False)
+        if(self.q0.qsize()>0):
+            data = self.q0.get(False)
+            csi = self.data_to_csi(data)
+            self.draw_amp(csi,0)
+
+        if(self.q1.qsize()>0):   
+            data = self.q1.get(False)
+            csi = self.data_to_csi(data)
+            self.draw_amp(csi,1)
+
+        if(self.q2.qsize()>0):
+            data = self.q2.get(False)
+            csi = self.data_to_csi(data)
+            self.draw_amp(csi,2)
+
+    def timer_cb(self):
+        print("q0 ",self.q0.qsize())
+        print("q1 ",self.q1.qsize())
+        print("q2 ",self.q2.qsize())
+        self.draw_amp_signal.emit()
+
+    def draw_amp(self, csi, idx):
+
+        x_idx, y_amp = self._csi.cal_amp(csi)
+        self.csi_amp.p1.setRange(xRange=[0, max(x_idx)])
+
+        if(idx == 0):
+            self.csi_amp.curve.clear()
+            self.csi_amp.curve.setData(x_idx, y_amp,pen='r')
+        if(idx == 1):
+            self.csi_amp.curve2.clear()
+            self.csi_amp.curve2.setData(x_idx, y_amp,pen='g')
+        if(idx == 2):
+            self.csi_amp.curve3.clear()
+            self.csi_amp.curve3.setData(x_idx, y_amp,pen='b')
+
+    def data_to_csi(self, data):
+        csi_status = data[0]
+        csi_matrix = data[1]
+
+        if (self.nr_idx < csi_status['nr'] and self.nc_idx < csi_status['nc']):
+            csi = csi_matrix[self.nr_idx][self.nc_idx]
         else:
-            self.testing_btn.setText("Test\nStart")
-            self.training_btn.setEnabled(True)
-            self.tb_sta1ip.setEnabled(True)
-            self.tb_sta1len.setEnabled(True)
-            self.tb_sta2ip.setEnabled(True)
-            self.tb_sta2len.setEnabled(True)
-            self.tb_sta3ip.setEnabled(True)
-            self.tb_sta3len.setEnabled(True)
-        return
+            csi = csi_matrix[0][0]
+
+        return csi
+
+    def draw_amp_and_display_text(self, data):
+
+        csi_status = data[0]
+        csi = self.data_to_csi(data[1])
+        draw_amp(csi,0)
+
+        chw = {0:'20', 3:'40'}
+        self.tstamp_t.setText('Time Stamp = {}'.format(csi_status['tstamp']))
+        self.channel_t.setText('Channel = {} MHz'.format(csi_status['channel']))
+        self.bw_t.setText('Bandwidth = {} MHz'.format(chw[csi_status['chanBW']]))
+        self.rate_t.setText('Rate code = 0x{:02X}'.format(csi_status['rate']))
+        self.rssi_t.setText('RSSI (combiled) = {}'.format(csi_status['rssi']))
+        self.rssi0_t.setText('RSSI Chain 0 = {}'.format(csi_status['rssi_0']))
+        self.rssi1_t.setText('RSSI Chain 1 = {}'.format(csi_status['rssi_1']))
+        self.rssi2_t.setText('RSSI Chain 2 = {}'.format(csi_status['rssi_2']))
+        self.nf_t.setText('Noise = {} dBm'.format(csi_status['noise']))
+ 
 
     def update(self, data):
         try:
             if (self.pause):
                 return
             csi_status = data[0]
-            csi_matrix = data[1]
-            if (self.nr_idx < csi_status['nr'] and self.nc_idx < csi_status['nc']):
-                csi = csi_matrix[self.nr_idx][self.nc_idx]
-            else:
-                csi = csi_matrix[0][0]
-
-            x_idx, y_amp, y_phase = self._csi.cal_amp_phase(csi)
-            esnr_bpsk, esnr_qpsk, esnr_16qam, esnr_64qam = self._csi.cal_esnr(csi)
-            x_cir, y_cir = self._csi.cal_pdp(csi)
-
-            self.esnr_text.setText('Effective SNR (64QAM) = {:.0f}\nEffective SNR (16QAM) = {:.0f}\nEffective SNR (QPSK)  = {:.0f}\nEffective SNR (BPSK)  = {:.0f}'\
-                .format(esnr_64qam, esnr_16qam, esnr_qpsk, esnr_bpsk))
-
-            chw = {0:'20', 3:'40'}
-            self.tstamp_t.setText('Time Stamp = {}'.format(csi_status['tstamp']))
-            self.channel_t.setText('Channel = {} MHz'.format(csi_status['channel']))
-            self.bw_t.setText('Bandwidth = {} MHz'.format(chw[csi_status['chanBW']]))
-            self.rate_t.setText('Rate code = 0x{:02X}'.format(csi_status['rate']))
-            self.rssi_t.setText('RSSI (combiled) = {}'.format(csi_status['rssi']))
-            self.rssi0_t.setText('RSSI Chain 0 = {}'.format(csi_status['rssi_0']))
-            self.rssi1_t.setText('RSSI Chain 1 = {}'.format(csi_status['rssi_1']))
-            self.rssi2_t.setText('RSSI Chain 2 = {}'.format(csi_status['rssi_2']))
-            self.nf_t.setText('Noise = {} dBm'.format(csi_status['noise']))
-            self.csi_amp.curve.setData(x_idx, y_amp)
-            self.csi_amp.setXRange(0, max(x_idx))
-            self.csi_phase.curve.setData(x_idx, y_phase)
-            self.csi_phase.setXRange(0, max(x_idx))
-            self.cir.curve.setData(x_cir, y_cir)
-            self.cir.setXRange(0, max(x_cir))
+            csi = self.data_to_csi(data)
+            if (len(csi)==114):
+                label = csi_status['payload_len']-88
+                queue_idx = label%3
+                if(queue_idx == 0):
+                    self.q0.put(data,False)
+                elif(queue_idx == 1):
+                    self.q1.put(data,False)
+                else:
+                    self.q2.put(data,False)
         
         except Exception as e:
             print(e)
